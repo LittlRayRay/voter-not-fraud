09f702f047f62fc57291403e8b9478db34b5f776
The Average Avocado <rayan.nadeem@outlook.com>
2024-03-19 22:30:50 +0000

    it works

--- voting-beano.py
+++ voting-beano.py
@@ -1,7 +1,11 @@
+import requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 #get counter from file
@@ -10,19 +14,37 @@ count = int(f.read())
 so_far = 0
 vote_limit_write = 1
 
+
+
+options = webdriver.ChromeOptions()
+
+options.add_extension('./captcha-solver.crx')
+
+driver = webdriver.Chrome(options)
+
 while True:
-    # try:
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--blink-settings=imagesEnabled=false')
-        driver = webdriver.Chrome(chrome_options)
+    try:
         driver.get("https://www.beano.com/posts/britains-funniest-class")
-        driver.implicitly_wait(2)
+        driver.implicitly_wait(3)
+
         cookie_button = driver.find_element(By.ID, "onetrust-reject-all-handler")
         cookie_button.click()
         school = driver.find_element(By.XPATH, "//*[@src='https://www.beano.com/wp-content/uploads/2023/03/BFC24_Joke-10.png?strip=all&quality=76&w=434']")
         school.click()
-        time.sleep(1.5)
-        driver.refresh()
+
+        time.sleep(2)
+
+        while True:
+            try:
+                div_element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div.refresh.button")))
+                pass
+            except:
+                break
+
+            
+
+        time.sleep(2)
+
         so_far += 1
         if so_far == vote_limit_write:
                 count += vote_limit_write
@@ -30,6 +52,8 @@ while True:
                 f = open("votecount.txt", "w")
                 f.write(f"{count}")
         print (count + so_far)
-        # wait_until = input("PRESS ENTER TO CONTINUE")
-    # except:
-    #     pass
+                # wait_until = input("PRESS ENTER TO CONTINUE")
+            # except:
+            #     pass
+    except:
+        pass
